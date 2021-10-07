6d5b09c10b975ea59577a208934a4d09b88694c2
Obez99 <giuliogx@live.it>
2021-10-07 16:03:57 +0200

    Adds alternative method to generate password

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,8 @@ const myColor = prompt("Perfetto! Ora dimmi... Qual'è il tuo colore preferito?"
 
 alert("Fantastico! Fammi pensare...");
 alert("Attenti ancora un momento...");
-alert("Ce l'ho! La tua password è: " + myName + mySurname + myColor + "21");
+
+//alert("Ce l'ho! La tua password è: " + myName + mySurname + myColor + "21");
+
+/*Alternative method*/
+alert(`Ce l'ho! La tua password è: ${myName}${mySurname}${myColor}21`);
