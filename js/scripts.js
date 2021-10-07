4e93c641a2494a9e31bd196010ad2030013ae904
Obez99 <giuliogx@live.it>
2021-10-07 15:58:38 +0200

    Completes main task

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-alert("Benvenuto nel mio generatore di password!")
+alert("Benvenuto nel mio generatore di password!");
+
+const myName = prompt("Per cominciare, come ti chiami?");
+const mySurname = prompt("Che bel nome! Adesso ho bisogno del tuo cognome.");
+const myColor = prompt("Perfetto! Ora dimmi... Qual'è il tuo colore preferito?");
+
+alert("Fantastico! Fammi pensare...");
+alert("Attenti ancora un momento...");
+alert("Ce l'ho! La tua password è: " + myName + mySurname + myColor + "21");
