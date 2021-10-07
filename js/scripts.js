ffa3482d021b75e2d22c118d71621483ff82288a
Obez99 <giuliogx@live.it>
2021-10-07 20:22:55 +0200

    Adds a third method

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,15 @@ alert("Benvenuto nel mio generatore di password!");
 const myName = prompt("Per cominciare, come ti chiami?");
 const mySurname = prompt("Che bel nome! Adesso ho bisogno del tuo cognome.");
 const myColor = prompt("Perfetto! Ora dimmi... Qual'è il tuo colore preferito?");
+let myPassword = myName+mySurname+myColor+"21";
 
 alert("Fantastico! Fammi pensare...");
 alert("Attenti ancora un momento...");
 
+/*Alternative method 1*/
 //alert("Ce l'ho! La tua password è: " + myName + mySurname + myColor + "21");
 
-/*Alternative method*/
-alert(`Ce l'ho! La tua password è: ${myName}${mySurname}${myColor}21`);
+/*Alternative method 2*/
+//alert(`Ce l'ho! La tua password è: ${myName}${mySurname}${myColor}21`);
+
+alert("Ce l'ho! La tua password è: " + myPassword);
